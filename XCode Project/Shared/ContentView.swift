5e22a8b707a4221073c21ab1b3eaca653f01186d
Joe Buchoff <joebuchoff@gmail.com>
2021-12-11 13:58:47 -0600

    Created home screen view.

--- XCode Project/Shared/ContentView.swift
+++ XCode Project/Shared/ContentView.swift
@@ -10,15 +10,74 @@ import CoreData
 
 struct ContentView: View
 {
-    @Environment(\.managedObjectContext) private var viewContext
-
+    @Environment(\.managedObjectContext)    private var context
+    @FetchRequest(sortDescriptors: [])      private var categories: FetchedResults<Category>
+    
     var body: some View
     {
-        NavigationView
+        ZStack
         {
-            List
+            bgGradient()
+            homeScreen()
+        }
+    }
+    
+    // Refactored Views
+    fileprivate func bgGradient(_ color1: Color = .red, _ color2: Color = .yellow) -> some View
+    {
+        return LinearGradient(colors: [.red, .yellow], startPoint: .bottomLeading, endPoint: .topTrailing)
+        .ignoresSafeArea()
+    }
+
+    fileprivate func button(text: String) -> some View
+    {
+        return Button
+        {
+            
+        } label: {
+            Text(text)
+                .frame(width: 280, height: 40, alignment: .center)
+                .background(.white)
+                .cornerRadius(30)
+                .foregroundColor(.black)
+        }
+    }
+    
+    // Refactored whole screens
+    fileprivate func homeScreen() -> some View
+    {
+        return VStack
+        {
+            HStack
             {
+                Text("Profile Pic")
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .background(.white)
+                VStack
+                {
+                    Text("Name")
+                        .font(.system(size:20, weight: .black))
+                        .padding(1)
+                    Text("X Goals Hit")
+                }.padding()
+            }.padding()
+            
+            Spacer()
+            
+            VStack
+            {
+                Text("Quick Actions:")
+                    .font(.system(size:25, weight: .bold))
+                    .foregroundColor(.white)
+                button(text: "Record Measurement")
+                button(text: "Add Metric to Track")
+                button(text: "Add Category")
             }
+            
+            Spacer()
+            
+            button(text: "My Categories")
+                .padding()
         }
     }
 }
--- XCode Project/Shared/DataController.swift
+++ XCode Project/Shared/DataController.swift
@@ -5,4 +5,24 @@
 //  Created by Joe Buchoff on 12/5/21.
 //
 
+import CoreData
 import Foundation
+
+class DataController: ObservableObject
+{
+    let container = NSPersistentContainer(name: "Track_Anything")
+    
+    init()
+    {
+        container.loadPersistentStores
+        { description, error in
+            if let error = error
+            {
+                print("CoreData failed to load: \(error.localizedDescription)")
+                return
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
